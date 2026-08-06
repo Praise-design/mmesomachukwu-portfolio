3f9931d76a9e5f941c517cddf5d164ff6385a99b
Praise-design <chibuokemmesomachukwu@gmail.com>
2026-08-07 00:02:27 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Portfolio loaded");
+// Smooth scrolling for navigation links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener('click', function(e) {
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+
+            behavior: 'smooth'
+
+        });
+
+    });
+
+});
+
+
+// Highlight navigation while scrolling
+
+window.addEventListener('scroll', () => {
+
+    const nav = document.querySelector('.navbar');
+
+    if(window.scrollY > 50){
+
+        nav.style.background = "#061324";
+
+        nav.style.boxShadow = "0 5px 20px rgba(0,0,0,.2)";
+
+    }
+
+    else{
+
+        nav.style.background = "transparent";
+
+        nav.style.boxShadow = "none";
+
+    }
+
+});
